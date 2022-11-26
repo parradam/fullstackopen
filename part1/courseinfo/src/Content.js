30b5aae8a8fc2becc2f7ca231afd683cfef5e51a
parradam <69434868+parradam@users.noreply.github.com>
2022-11-26 00:31:31 +0000

    1.4 complete

--- part1/courseinfo/src/Content.js
+++ part1/courseinfo/src/Content.js
@@ -4,9 +4,9 @@ import Part from './Part'
 function Content(props) {
   return (
     <>
-      <Part part={props.part1} />
-      <Part part={props.part2} />
-      <Part part={props.part3} />
+      <Part part={props.parts[0]} />
+      <Part part={props.parts[1]} />
+      <Part part={props.parts[2]} />
     </>
   )
 }
