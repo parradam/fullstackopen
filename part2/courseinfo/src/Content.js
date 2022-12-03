e987550f501e7971673886910949454e3db9862f
parradam <69434868+parradam@users.noreply.github.com>
2022-12-03 21:17:19 +0000

    2.1-2.3 complete

---
+++ part2/courseinfo/src/Content.js
@@ -0,0 +1,9 @@
+import React from "react"
+import Part from "./Part"
+
+const Content = ({ parts }) => 
+  <>
+    {parts.map(part => <Part part={part} key={part.id} />)}
+  </>
+
+  export default Content
---
+++ part2/courseinfo/src/Course.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Header from './Header'
+import Content from './Content'
+import Total from './Total'
+
+const Course = ({ course }) => {
+  
+  const totalExercises = course.parts.reduce((sum, part) => sum + part.exercises, 0)
+  
+  return (
+    <>
+      <Header title={course.name} />
+      <Content parts={course.parts} />
+      <Total sum={totalExercises} />
+    </>
+  )
+}
+
+export default Course
