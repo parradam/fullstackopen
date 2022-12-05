2314a15bdc103a1dfd6fd8b7cfa46c2302330dfb
parradam <69434868+parradam@users.noreply.github.com>
2022-12-05 20:25:57 +0000

    2.8 complete

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -5,15 +5,21 @@ const App = () => {
     { name: 'Arto Hellas' }
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const handleNameChange = (e) => {
     setNewName(e.target.value)
+  }
+
+  const handleNumberChange = (e) => {
+    setNewNumber(e.target.value)
   }
 
   const handleSubmit = (e) => {
     e.preventDefault()
     const personToAdd = {
       name: newName,
+      number: newNumber
     }
 
     const personExists = persons.some(person => person.name === personToAdd.name)
@@ -25,18 +31,16 @@ const App = () => {
 
     setPersons(persons.concat(personToAdd))
     setNewName('')
+    setNewNumber('')
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          name: <input value={newName} onChange={handleNameChange} />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
+      <form onSubmit={handleSubmit} style={{display: "flex", flexDirection: "column"}}>
+        <div>name: <input value={newName} onChange={handleNameChange} /></div>
+        <div>number: <input value={newNumber} onChange={handleNumberChange} /></div>
+        <div><button type="submit">add</button></div>
       </form>
       <h2>Numbers</h2>
       <ul>
