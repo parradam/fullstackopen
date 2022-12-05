50af30d4214cf7723ec354281587cc3c33f990f3
parradam <69434868+parradam@users.noreply.github.com>
2022-12-05 21:14:07 +0000

    2.9 complete

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,10 +2,16 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
+  ])
+  const [nameFilter, setNameFilter] = useState('')
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+
+  const personsToShow = persons.filter(person => person.name.toLocaleLowerCase().includes(nameFilter.toLocaleLowerCase()))
+
+  const handleNameFilterChange = (e) => {
+    setNameFilter(e.target.value)
+  }
 
   const handleNameChange = (e) => {
     setNewName(e.target.value)
@@ -36,15 +42,20 @@ const App = () => {
 
   return (
     <div>
-      <h2>Phonebook</h2>
+      <h1>Phonebook</h1>
+      <div>
+        <h2>Filter</h2>
+        name: <input value={nameFilter} onChange={handleNameFilterChange} />
+      </div>
       <form onSubmit={handleSubmit} style={{display: "flex", flexDirection: "column"}}>
+        <h2>Add new</h2>
         <div>name: <input value={newName} onChange={handleNameChange} /></div>
         <div>number: <input value={newNumber} onChange={handleNumberChange} /></div>
         <div><button type="submit">add</button></div>
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map(person => <li key={person.name}>{person.name}</li>)}
+        {personsToShow.map(person => <li key={person.name}>{person.name}: {person.number}</li>)}
       </ul>
     </div>
   )
