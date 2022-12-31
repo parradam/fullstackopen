f20f52340c665b7ea2162bbaf8987974ba9991e3
Adam Parr <adamparr.uk@gmail.com>
2022-12-31 19:14:24 +0000

    3.17-3.18 complete

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -8,7 +8,7 @@ const app = express()
 const requestLogger = (request, response, next) => {
     console.log('Method:', request.method)
     console.log('Path:', request.path)
-    console.log('Body:', request.body)
+    console.log('Request body:', request.body)
     next()
 }
 
@@ -16,29 +16,6 @@ app.use(cors())
 app.use(express.static('build'))
 app.use(express.json())
 app.use(requestLogger)
-
-let phonebook = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
 
 app.get('/api/persons', (request, response) => {
     Person.find({}).then(persons => {
@@ -67,13 +44,6 @@ app.post('/api/persons', (request, response) => {
         })
     }
 
-    const nameExists = phonebook.some(p => p.name === person.name)
-    if (nameExists) {
-        return response.status(400).json({
-            error: 'name must be unique'
-        })
-    }
-
     const newPerson = new Person({
         name: person.name,
         number: person.number,
@@ -84,46 +54,59 @@ app.post('/api/persons', (request, response) => {
     })
 })
 
+app.put('/api/persons/:id', (request, response, next) => {
+    const detailsToUpdate = {
+        number: request.body.number,
+    }
+
+    Person.findByIdAndUpdate(request.params.id, detailsToUpdate, { new: true })
+      .then(updatedPerson => {
+        response.json(updatedPerson)
+      })
+      .catch(error => {
+        next(error)
+      })
+})
+
 app.delete('/api/persons/:id', (request, response, next) => {
-    console.log('deleting')
     Person.findByIdAndDelete(request.params.id)
       .then(person => {
         response.status(204).end()
       })
       .catch(error => {
         next(error)
-        // console.log(error.name, error.message)
-        // response.statusMessage = "Delete unsucessful. No person found with this ID"
-        // response.status(404).end()
       })
 })
 
-app.get('/info', (request, response) => {
-    const report = `<div>
-                        <p>Phonebook has info for ${phonebook.length} people</p>
-                        <p>${new Date()}<p>
-                    </div>`
-    
-    response.send(report)
+app.get('/info', (request, response, next) => {
+    Person.count({})
+      .then(count => {
+          const report = `<div>
+                           <p>Phonebook has info for ${count} people</p>
+                           <p>${new Date()}<p>
+                          </div>`
+
+          response.send(report)
+      })
+      .catch(error => next(error))
 })
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
 
 const errorHandler = (error, request, response, next) => {
     console.log(error.message)
 
-    if (error.name === 'CastError') {
-        if (request.method === 'GET') return response.status(400).send({ error: 'incorrectly formatted ID' })
-        if (request.method === 'DELETE') {
-            response.statusMessage = "Delete unsucessful. No person found with this ID"
-            response.status(404).send()
-        }
-    }
-
+    if (error.name === 'CastError') return response.status(400).send({ error: 'incorrectly formatted ID' })
     next(error)
 }
 
 app.use(errorHandler)
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
