3f6e62d5ab609810292a228abc6532ccc69ae61a
Adam Parr <adamparr.uk@gmail.com>
2023-01-04 01:14:11 +0000

    4.5 complete

--- part4/blog-list/utils/list_helper.js
+++ part4/blog-list/utils/list_helper.js
@@ -7,7 +7,12 @@ const totalLikes = (blogs) => {
     return blogs.reduce((sumOfLikes, blog) => sumOfLikes += blog.likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+    return blogs.reduce((a, b) => a.likes > b.likes ? a : b, {})
+}
+
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
 }
