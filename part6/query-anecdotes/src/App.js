9593e0fb06ec9041cb68fc7ec56dd415f8b14c70
Adam Parr <adamparr.uk@gmail.com>
2023-04-02 18:28:24 +0100

    6.20 complete

---
+++ part6/query-anecdotes/src/App.js
@@ -0,0 +1,48 @@
+import { useQuery } from 'react-query'
+import axios from 'axios'
+
+import AnecdoteForm from './components/AnecdoteForm'
+import Notification from './components/Notification'
+
+const App = () => {
+    const result = useQuery('anecdotes', () =>
+        axios.get('http://localhost:3001/anecdotes').then((res) => res.data)
+    )
+    const anecdotes = result.data
+    console.log(anecdotes)
+
+    const handleVote = (anecdote) => {
+        console.log('vote')
+    }
+
+    if (result.isLoading) {
+        return <div>Loading data...</div>
+    }
+
+    if (result.isError) {
+        return <div>Ancedote service not available</div>
+    }
+
+    return (
+        <div>
+            <h3>Anecdote app</h3>
+
+            <Notification />
+            <AnecdoteForm />
+
+            {anecdotes.map((anecdote) => (
+                <div key={anecdote.id}>
+                    <div>{anecdote.content}</div>
+                    <div>
+                        has {anecdote.votes}
+                        <button onClick={() => handleVote(anecdote)}>
+                            vote
+                        </button>
+                    </div>
+                </div>
+            ))}
+        </div>
+    )
+}
+
+export default App
